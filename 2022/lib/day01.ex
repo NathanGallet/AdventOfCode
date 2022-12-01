ce4c65587e7eb300d39f95daa4d122e4f5d63bab
Nathan Gallet <nathan.gallet@thetrainline.com>
2022-12-01 13:52:14 +0100

    Day01

--- 2022/lib/day01.ex
+++ 2022/lib/day01.ex
@@ -2,14 +2,33 @@ defmodule Day01 do
   @moduledoc false
 
   def run do
-    "day01"
-    |> Aoc.read_input_as_list_of_integer!()
-    |> part1()
+    input =
+      "./lib/inputs/day01.txt"
+      |> File.read!()
+      |> parse_input()
+
+    IO.puts("Value of part1 : #{part1(input)}")
+    IO.puts("Value of part2 : #{part2(input)}")
   end
 
-  def part1(input) do
+  def parse_input(input) do
     input
-    |> Enum.chunk_every(2, 1, :discard)
-    |> Enum.count(fn [left, right] -> right > left end)
+    |> String.split("\n")
+    |> Enum.chunk_by(&(&1 == ""))
+    |> Enum.reject(&(&1 == [""]))
+    |> Enum.map(fn elf_calories ->
+      elf_calories
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.sum()
+    end)
+  end
+
+  def part1(input), do: Enum.max(input)
+
+  def part2(input) do
+    input
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.sum()
   end
 end
--- 2022/test/day01_test.exs
+++ 2022/test/day01_test.exs
@@ -2,19 +2,26 @@ defmodule Day01Test do
   use ExUnit.Case
 
   test "day01-part1" do
-    input = [
-      199,
-      200,
-      208,
-      210,
-      200,
-      207,
-      240,
-      269,
-      260,
-      263
-    ]
+    input =
+      """
+      1000
+      2000
+      3000
 
-    assert Day01.part1(input) == 7
+      4000
+
+      5000
+      6000
+
+      7000
+      8000
+      9000
+
+      10000
+      """
+      |> Day01.parse_input()
+
+    assert Day01.part1(input) == 24000
+    assert Day01.part2(input) == 45000
   end
 end
